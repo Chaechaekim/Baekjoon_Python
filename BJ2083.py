4571f7d9ba8f9ed0e19763254581b88ab81a47a6
chaechaekim <chaeeun7478@naver.com>
2025-07-27 23:20:45 +0900

    Add BJ2083.py

---
+++ BJ2083.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+while True:
+    name,age,weight=map(str,input().rstrip().split())
+    age=int(age)
+    weight=int(weight)
+    if name=='#' and age==0 and weight==0:
+        break
+    if age>17 or weight>=80:
+        print(name+' Senior')
+    else:print(name+' Junior')
